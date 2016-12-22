f9ffd95d07f58657aa5ff891569004f0f63b9214
Alec <freakyfactoid@gmail.com>
2016-12-21 18:37:55 -0800

    Initial commit

---
+++ app/src/main/java/alec/is/awesome/sevenwondersscorecalculator/Input.java
@@ -0,0 +1,217 @@
+package alec.is.awesome.sevenwondersscorecalculator;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.Enumeration;
+
+
+public class Input extends ActionBarActivity {
+    int stage = 0;
+    int score = 0;
+    int gear = 0;
+    int compass = 0;
+    int clay = 0;
+    String name = "";
+
+    EditText input;
+    TextView text;
+    ImageView img;
+    ScoreCalculator.Expansions type = new ScoreCalculator.Expansions();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+        input = (EditText) findViewById(R.id.input);
+        text = (TextView) findViewById(R.id.text);
+        img = (ImageView) findViewById(R.id.imageView);
+        Intent i = getIntent();
+        type = i.getParcelableExtra("type");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_input, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable)drawable).getBitmap();
+        }
+
+        int width = drawable.getIntrinsicWidth();
+        width = width > 0 ? width : 1;
+        int height = drawable.getIntrinsicHeight();
+        height = height > 0 ? height : 1;
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    public void submit(View view){
+        Bitmap bit = null;
+        switch (stage){
+            case 0: name = input.getText().toString();
+                input.setInputType(InputType.TYPE_NUMBER_FLAG_SIGNED);
+                text.setText("VP Military");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card1);
+                break;
+            case 1: score += getNum(input);
+                text.setText("# of Coins");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card2);
+                break;
+            case 2: score += ((getNum(input) < 0)? getNum(input) : getNum(input) / 3);
+                text.setText("VP Stages of Wonder");
+                input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                //bit = BitmapFactory.decodeResource(getResources(), R.drawable.coolio);
+                break;
+            case 3: score += getNum(input);
+                text.setText("VP Blue Cards");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card4);
+                break;
+            case 4: score += getNum(input);
+                text.setText("VP Yellow Cards");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card5);
+                break;
+            case 5: score += getNum(input);
+                text.setText("Science Gears");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card6);
+                break;
+            case 6: gear = getNum(input);
+                text.setText("Science Compass");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card7);
+                break;
+            case 7: compass = getNum(input);
+                text.setText("Science Clay Tablet");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card8);
+                break;
+            case 8: clay = getNum(input);
+                text.setText("VP Purple Cards");
+                bit = BitmapFactory.decodeResource(getResources(), R.drawable.card9);
+                break;
+            case 9: score += getNum(input);
+                if(!type.projects && !type.babel && !type.leaders && !type.cities) {
+                    doScience(7);
+                    end();
+                }
+                if(type.cities){
+                    text.setText("VP Black Cards");
+                    bit = BitmapFactory.decodeResource(getResources(), R.drawable.card10);
+                    break;
+                }
+            case 10:
+                if(type.leaders){
+                    score += getNum(input);
+                    text.setText("VP Leaders");
+                    stage = 10;
+                    bit = BitmapFactory.decodeResource(getResources(), R.drawable.card11);
+                    break;
+                }
+            case 11:
+                if(type.babel){
+                    score += getNum(input);
+                    text.setText("Babel Tiles");
+                    stage = 11;
+                    break;
+                }
+            case 12:
+                if(type.projects) {
+                    score += (type.babel)? getNum(input) * 2 : getNum(input);
+                    text.setText("VP from projects");
+                    stage = 12;
+                    break;
+                }
+            case 13: score += (type.babel)? getNum(input) * 2 : getNum(input);
+                if(type.projects) {
+                    text.setText("Project Science Token");
+                    bit = BitmapFactory.decodeResource(getResources(), R.drawable.card12);
+                    stage = 13;
+                    break;
+                } else {
+                    doScience(7);
+                    end();
+                }
+            case 14:
+                if(type.projects){
+                    int i = getNum(input);
+                    doScience(i * 3 + 7);
+                } else {
+                    score += (type.babel)? getNum(input) * 2 : getNum(input);
+                }
+                end();
+        }
+        img.setImageBitmap(bit);
+        input.setText("");
+        stage++;
+    }
+
+    public void doScience(int mult){
+        score += clay * clay;
+        score += compass * compass;
+        score += gear * gear;
+        if(gear <= compass && gear <= clay){
+            score += mult * gear;
+        } else if(compass <= gear && compass <= clay){
+            score += mult * compass;
+        } else {
+            score += mult * clay;
+        }
+    }
+
+    public void end(){
+        Intent i = new Intent();
+        i.putExtra("score", score);
+        i.putExtra("name", name);
+        setResult(0, i);
+        finish();
+    }
+
+    public int getNum(EditText eddy){
+        int i = 0;
+        try{
+            i = Integer.parseInt(eddy.getText().toString());
+        } catch (Exception e){
+            return 0;
+        }
+        return i;
+    }
+}
---
+++ app/src/main/java/alec/is/awesome/sevenwondersscorecalculator/ScoreCalculator.java
@@ -0,0 +1,139 @@
+package alec.is.awesome.sevenwondersscorecalculator;
+
+import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+
+public class ScoreCalculator extends ActionBarActivity {
+
+    TextView main;
+    Expansions type = new Expansions();
+    CheckBox cities;
+    CheckBox leaders;
+    CheckBox projects;
+    CheckBox babel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_calculator);
+        main  = (TextView) findViewById(R.id.textMain);
+        cities = (CheckBox) findViewById(R.id.cities);
+        leaders = (CheckBox) findViewById(R.id.leaders);
+        projects = (CheckBox) findViewById(R.id.projects);
+        babel = (CheckBox) findViewById(R.id.babel);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_score_calculator, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startInput(View view){
+        Intent i = new Intent(this, Input.class);
+        i.putExtra("type", type);
+        startActivityForResult(i, 0);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        String original = main.getText().toString();
+        try {
+            int i = data.getIntExtra("score", -1);
+            String s = data.getStringExtra("name");
+            main.setText(original + System.getProperty("line.separator") + s + ": " + i);
+        } catch(Exception e){
+            main.setText(original);
+        }
+    }
+
+    public static class Expansions implements Parcelable{
+        public boolean cities = false;
+        public boolean leaders = false;
+        public boolean babel = false;
+        public boolean projects = false;
+
+        public Expansions(){}
+
+        public int describeContents() {
+            return 0;
+        }
+
+        // write your object's data to the passed-in Parcel
+        public void writeToParcel(Parcel out, int flags) {
+            boolean [] bools = {cities, leaders, babel, projects};
+            out.writeBooleanArray(bools);
+        }
+
+        // this is used to regenerate your object. All Parcelables must have a CREATOR that implements these two methods
+        public static final Parcelable.Creator<Expansions> CREATOR = new Parcelable.Creator<Expansions>() {
+            public Expansions createFromParcel(Parcel in) {
+                return new Expansions(in);
+            }
+
+            public Expansions[] newArray(int size) {
+                return new Expansions[size];
+            }
+        };
+
+        // example constructor that takes a Parcel and gives you an object populated with it's values
+        private Expansions(Parcel in) {
+            boolean[] bools = new boolean[4];
+            in.readBooleanArray(bools);
+            this.cities = bools[0];
+            this.leaders = bools[1];
+            this.babel = bools[2];
+            this.projects = bools[3];
+        }
+    }
+
+    public void babel(View view) {
+        type.babel = (type.babel)? false : true;
+    }
+
+    public void leaders(View view){
+        type.leaders = (type.leaders)? false :true;
+    }
+
+    public void cities(View view){
+       type.cities = (type.cities)? false :true;
+    }
+
+    public void projects(View view){
+        type.projects = (type.projects)? false :true;
+    }
+
+    public void reset(View view){
+        main.setText("Scores");
+        type = new Expansions();
+        babel.setChecked(false);
+        projects.setChecked(false);
+        leaders.setChecked(false);
+        cities.setChecked(false);
+    }
+}
